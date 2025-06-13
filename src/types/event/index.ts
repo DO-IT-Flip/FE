a00a9bf6fbb8621e15b269bbd174c66983238a33
forest914 <85431651+forest914@users.noreply.github.com>
2025-06-13 20:16:31 +0900

    fontawesome

--- src/types/event/index.ts
+++ src/types/event/index.ts
@@ -2,7 +2,7 @@ export interface CalendarEvent {
   //일정 하나에 대한 정보
   id: string; //일정 구분을 위한 식별자
   title: string;
-  description?: string; //일정의 상세 설명
+  description: string; //일정의 상세 설명
   start: string; // 날짜 문자열 e.g., "2025-01-02T10:00:00"
   end: string;
   allDay: boolean;
