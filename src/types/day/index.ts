2ce230c08266ff48a17e779f1160a330646e062a
forest914 <85431651+forest914@users.noreply.github.com>
2025-06-13 18:35:01 +0900

    types

--- src/types/day/index.ts
+++ src/types/day/index.ts
@@ -1,8 +1,9 @@
-import { CalendarEvent } from "../calendar_event";
+import { CalendarEvent } from "../event";
 
 export interface CalendarDay {
+  //하루에 해당하는 정보
   date: string; // "2025-01-02"
   events: CalendarEvent;
-  isToday?: boolean;
-  isCurrentMonth?: boolean;
+  isToday: boolean;
+  isCurrentMonth: boolean;
 }
--- src/types/event/index.ts
+++ src/types/event/index.ts
@@ -1,9 +1,10 @@
 export interface CalendarEvent {
-  id: string;
+  //일정 하나에 대한 정보
+  id: string; //일정 구분을 위한 식별자
   title: string;
-  description?: string;
-  start: string; // ISO 날짜 문자열 e.g., "2025-01-02T10:00:00"
-  end: string; // ISO 날짜 문자열
-  allDay?: boolean;
-  category?: string; // 개인/업무/기타 등 분류
+  description?: string; //일정의 상세 설명
+  start: string; // 날짜 문자열 e.g., "2025-01-02T10:00:00"
+  end: string;
+  allDay: boolean;
+  category: string;
 }
